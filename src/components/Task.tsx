c6e5d977c1ecbc851d8b3d7c257cf0ffdbf94de0
KelwinGerson <83376362+KelwinGerson@users.noreply.github.com>
2022-10-11 16:05:35 -0300

    feat: component

---
+++ src/components/Task.tsx
@@ -0,0 +1,9 @@
+export function Taks() {
+    return (
+        <div>
+            <strong>Tarefas criadas</strong>
+            <p></p>
+            <strong>Concluídas</strong>
+        </div>
+    )
+}
