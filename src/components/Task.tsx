5b4b6f31861934c76617ccbab47ab54f70ecb0be
KelwinGerson <83376362+KelwinGerson@users.noreply.github.com>
2022-10-13 18:42:43 -0300

    delete

--- src/components/Task.tsx
+++
@@ -1,9 +0,0 @@
-export function Taks() {
-    return (
-        <div>
-            <strong>Tarefas criadas</strong>
-            <p></p>
-            <strong>Concluídas</strong>
-        </div>
-    )
-}
