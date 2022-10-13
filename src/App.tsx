10b96e9a79f62528e9ce6a9f740e1822c44e91ec
KelwinGerson <83376362+KelwinGerson@users.noreply.github.com>
2022-10-13 18:41:40 -0300

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 // import './App.css'
 import { Header } from './components/Header'
-import { Taks } from './components/Task';
+import { NewTask } from './components/NewTask'
+
 import './global.css';
 
 function App() {
   return (
     <div>
       <Header/>
-      <Taks/>
+      <NewTask/>
     </div>
   )
 }
