f026b36f15cacedde222b522c185ad1d739ac1c8
KelwinGerson <83376362+KelwinGerson@users.noreply.github.com>
2022-10-14 17:38:56 -0300

    refactor: newtask

--- src/components/NewTask.tsx
+++ src/components/NewTask.tsx
@@ -1,14 +1,15 @@
+import { Plus, Scroll } from 'phosphor-react'
 import { ChangeEvent, FormEvent, InvalidEvent, useState } from 'react'
 import styles from '../components/NewTask.module.css'
 import { LiskTask } from './ListTask'
 
-export function NewTask () {
+export function NewTask() {
 
     const [tasks, setTasks] = useState([
         ''
     ])
 
-    const [newTask, setNewTask ] = useState('')
+    const [newTask, setNewTask] = useState('')
 
     function handleCreateNewTask(event: FormEvent) {
         event?.preventDefault()
@@ -16,7 +17,7 @@ export function NewTask () {
         setNewTask('');
     }
 
-    function handleNewTaskChange (event: ChangeEvent<HTMLTextAreaElement>) {
+    function handleNewTaskChange(event: ChangeEvent<HTMLTextAreaElement>) {
         event.target.setCustomValidity('')
         setNewTask(event.target.value)
     }
@@ -27,31 +28,52 @@ export function NewTask () {
 
     return (
         <article>
-            <form onSubmit={handleCreateNewTask} className={styles.boxNewTask} action="">
-                    <textarea className={styles.textAreaTask}
-                        name='task'
-                        placeholder="Adicione uma nova tarefa..."
-                        value={newTask}
-                        onChange={handleNewTaskChange}
-                        onInvalid={handleNewTaskInvalid}
-                        required
-                    />
+            <header className={styles.infoHeader}>
+                <div className={styles.taskCreated}>
+                    Tarefas criadas 0
+                </div>
+                <div className={styles.taskDone}>
+                    Concluídas 0
+                </div>
+            </header>
+            {/*                 
+                THIS IS APPER IF DON'T EXIST WITHOUT TASK
+                <Scroll size={50} />
+                <div className={styles.pendentTask}>
+                    Você ainda não tem tarefas cadastradas
+                </div>
+                <div className={styles.pendentTask}>
+                    Crie tarefas e organize seus itens a fazer
+                </div> */}
+
+            {/* CREATE A NEW TASK */}
+            <form onSubmit={handleCreateNewTask} className={styles.boxNewTask}>
+                <textarea className={styles.textAreaTask}
+                    name='task'
+                    placeholder="Adicione uma nova tarefa..."
+                    value={newTask}
+                    onChange={handleNewTaskChange}
+                    onInvalid={handleNewTaskInvalid}
+                    // required text on comment box
+                    required
+                />
+                {/** BOTÃO */}
                 <footer>
-                    <button className={styles.buttonCreate} type='submit' >
-                        <img src="src/assets/Layer 2.png" alt="" />
+                    <button className={styles.buttonCreate} type='submit' value='send'>
+                        <Plus size={20} />
                         Criar
                     </button>
                 </footer>
             </form>
-
+            
+            {/* TASK LIST */}
             <div className={styles.taskList}>
                 {tasks.map(task => {
                     return (
                         <LiskTask
                             key={task}
-                            content={task} onDeleteTaks={function (taks: string): void {
-                                throw new Error('Function not implemented.')
-                            } }                        
+                            content={task} 
+                            onDeleteTaks={function (taks: string): void {throw new Error('Function not implemented.')}}
                         />
                     )
                 })}
