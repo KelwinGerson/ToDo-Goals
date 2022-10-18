e4c1e4c5f6ded8aff6b2140d2acc31c5f28e5c7a
KelwinGerson <83376362+KelwinGerson@users.noreply.github.com>
2022-10-18 07:37:48 -0300

    refactor code

--- src/components/ListTask.tsx
+++ src/components/ListTask.tsx
@@ -2,32 +2,53 @@ import styles from './ListTask.module.css'
 import { CheckCircle, Trash } from 'phosphor-react'
 import { Checkbox } from 'pretty-checkbox-react';
 
+
 interface ListTaskProps {
+    //id = content 
+    id: string;
     content: string;
+    isCompleted: boolean;
     onDeleteTask: (task: string) => void;
+    onChangeIsComplete: (task: string) => void;
 }
 
-export function LiskTask({content, onDeleteTask} : ListTaskProps) {
+export function ListTask({id, isCompleted, content, onDeleteTask, onChangeIsComplete} : ListTaskProps) {
 
     function handleDeleteComment () {
-        onDeleteTask(content)
+        onDeleteTask(content);
+    }
+
+    function handleChangeIsComplete() {
+        onChangeIsComplete(content);
     }
 
     return (
-        <div className={styles.task} >
-            <div className={styles.commentBox}>
-                <div className={styles.commentContent}>
-                    <button className={styles.buttonDelete} onClick={handleDeleteComment} title='Deletar comentário'>
-                        <Trash size={15} color="#6495ED" />
-                        {/* 2º option */}
-                        <Checkbox shape="round" icon={<i className="mdi mdi-check" />}></Checkbox>                    
-                    </button>
-                    <button className={styles.buttonCheck}>
-                        <CheckCircle size={15} color="#6495ED" />
-                    </button>
-                </div>
-            </div>
-            <p>{content}</p>
+        <div>
+            <ul>
+                <li key={content} className={styles.todos}>
+                    <div className={styles.tasks}>
+
+                        <input 
+                            title='checkbox'
+                            type="checkbox"
+                            id={content}
+                            onClick={handleChangeIsComplete}
+                        />
+
+                        <label>
+                            <p>{content}</p>
+                        </label>
+
+                        <button
+                            type='button'
+                            title='delete'
+                            onClick={handleDeleteComment}
+                        >
+                            <Trash size={24} />                        
+                        </button>
+                    </div>
+                </li>
+            </ul>
         </div>
     )
 }
